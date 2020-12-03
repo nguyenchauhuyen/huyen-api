78e99df22a5dba7dc1365a55d3924a8e5fa6fb21
HUYEN NGUYEN <nguyenchauhuyen@gmail.com>
2020-12-03 13:16:31 +0700

    Booking Email

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
+// const _ = require("lodash");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+// const emailTemplate = {
+//   subject: "New Order at SimDep4G.com !!!",
+//   text: `New order created!
+//       Your account is now linked with: <%= order.customerName %>.`,
+//   html: `<h1>Welcome on mywebsite.fr!</h1>
+//       <p>Your account is now linked with: <%= order.customerName %>.<p>`
+// };
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity = await strapi.services.order.create(ctx.request.body);
+    if (entity.id) {
+      await strapi.plugins["email"].services.email.send({
+        to: "nguyenchauhuyen@gmail.com",
+        // from: "admin@strapi.io",
+        subject: "New Order at SimDep4G.com !!!",
+        text: `
+            The product ${entity.product.name} has been booked.
+            Customer Name: ${entity.customerName}
+            Customer Phone: ${entity.customerPhone}
+            Customer Address: ${entity.customerAddress}
+            Note:
+              ${entity.bookingNote}
+            `
+      });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  }
+};
