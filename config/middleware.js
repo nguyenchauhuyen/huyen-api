6aed8442c9d409db8688c42463a896cdedf35df1
HUYEN NGUYEN <nguyenchauhuyen@gmail.com>
2021-01-20 13:36:12 +0700

    cache product model

---
+++ config/middleware.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  settings: {
+    cache: {
+      enabled: true,
+      type: "mem",
+      max: 5000,
+      models: ["products"]
+      //   redisConfig: {
+      //     sentinels: [
+      //       { host: "192.168.10.41", port: 26379 },
+      //       { host: "192.168.10.42", port: 26379 },
+      //       { host: "192.168.10.43", port: 26379 }
+      //     ],
+      //     name: "redis-primary"
+      //   }
+    }
+  }
+});
