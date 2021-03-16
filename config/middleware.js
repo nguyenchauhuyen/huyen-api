2177048edb1f005f3fd2be1745ee1f80dda50dd1
HUYEN NGUYEN <nguyenchauhuyen@gmail.com>
2021-03-16 20:51:28 +0700

    update cache items

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     cache: {
       enabled: true,
       type: "mem",
-      max: 10000,
+      max: 5000,
       models: ["products"]
       //   redisConfig: {
       //     sentinels: [
