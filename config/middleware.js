8dfed89202580b790e651377a1f5be1b89a12151
Huyen Nguyen <nguyenchauhuyen@gmail.com>
2024-12-04 22:52:05 +0700

    config redis models

--- config/middleware.js
+++ config/middleware.js
@@ -2,16 +2,26 @@ module.exports = ({ env }) => ({
   settings: {
     cache: {
       enabled: true,
-      type: 'redis', // Use Redis for caching
+      type: env('REDIS_HOST') ? 'redis' : 'mem', // Use Redis for caching
       maxAge: 3600000 * 24, // 24 hours
+      max: 20000, // Safe value for 1 KB entries
       redisConfig: {
-        password: env('REDIS_PASSWORD'),
         host: env('REDIS_HOST'),
         port: env('REDIS_PORT'),
+        password: env('REDIS_PASSWORD'),
         db: 0,                             // Redis DB (default is 0)
         //ttl: 86400,                         // Cache TTL (Time to live) in seconds
       },
-      models: ['product', 'article'],
+      models: [
+        {
+          model: 'product',
+          maxAge: 3600000 * 24, // 24 hours
+        },
+        {
+          model: 'article',
+          maxAge: 3600000 * 24 * 30, // 24 hours
+        },
+      ],
     },
   },
 });
